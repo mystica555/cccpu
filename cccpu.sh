b7d324a569cdbe6f168a3aff110b3b2f7cba7cdc
Mike Hodson <mike@Ljomi>
2025-06-30 11:12:04 -0600

    What new changes do i have?

--- cccpu.sh
+++ cccpu.sh
@@ -2,7 +2,7 @@
 
 # #############################################################################
 #
-# Copyright (c) 2025, Mystica Online Technologies, Lakewood Colorado 
+# Copyright (c) 2025, Mike Hodson, Mystica Online Technologies, Lakewood Colorado 
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
